384f2cb6d299238bc00e9a113b252b613d6b8cfe
Anna Ossea <annaossea@gmail.com>
2022-08-29 12:00:04 -0700

    feat(form-wave): :arrow_up: move label letter-by-letter

--- form-wave/script.js
+++ form-wave/script.js
@@ -1,18 +1,34 @@
 let form = document.querySelectorAll('.form')[0];
 let labels = [];
 form.childNodes.forEach((node) => node.htmlFor && labels.push(node));
-console.log(labels)
+
+// Wrap each label letter in span
+labels.forEach((label) => {
+    let spanWrappedText = '';
+    for (let letter of label.innerHTML) {
+        spanWrappedText += `<span>${letter}</span>`
+    }
+    label.innerHTML = spanWrappedText;
+})
+
+
 form.addEventListener('focusin', animateLabelUp);
 form.addEventListener('focusout', animateLabelDown);
 
 function animateLabelUp(e) {
-    // labels
-    e.target.labels[0].classList.add('focused')
+    let transitionDelay = 100;
+    e.target.labels[0].childNodes.forEach((spanLetter) => {
+
+        spanLetter.style.transitionDelay = `${transitionDelay}ms`;
+        transitionDelay += 100;
+        spanLetter.classList.add('focused');
+
+    })
 }
 
 function animateLabelDown(e) {
     //only if there is no value in the input field
     if (!e.target.value.trim()) {
-        e.target.labels[0].classList.remove('focused');
+        e.target.labels[0].childNodes.forEach((spanLetter) => spanLetter.classList.remove('focused'))
     }
 }

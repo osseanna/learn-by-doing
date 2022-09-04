2202227c22bb714860d132d63ca03ab0190f92dc
Anna Ossea <annaossea@gmail.com>
2022-09-03 20:57:18 -0700

    refactor(hidden-search): use toogle method to add/remove class

--- hidden-search/script.js
+++ hidden-search/script.js
@@ -4,6 +4,6 @@ var searchInput = document.getElementById("search-input");
 searchBtn.addEventListener('click', toggleInputVisibility);
 
 function toggleInputVisibility() {
-    searchInput.classList.contains('visible') ? searchInput.classList.remove('visible') : searchInput.classList.add('visible');
+    searchInput.classList.toggle('visible');
     searchInput.focus()
 }

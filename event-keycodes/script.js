9c7f262dae7a3063cf4f88e77e26174e71030dca
Anna Ossea <annaossea@gmail.com>
2022-09-12 09:42:31 -1000

    feat(event-keycodes): display event key, keyCode and code in the UI

--- event-keycodes/script.js
+++ event-keycodes/script.js
@@ -1,4 +1,6 @@
 const containerDiv = document.querySelector('.container');
 document.addEventListener('keydown', (e) => {
-    console.log(e)
+    containerDiv.innerHTML = `<div class="box"><p>event.key</p> <div>${e.key}</div></div>
+                            <div class="box"><p>event.keyCode</p> <div>${e.keyCode}</div></div>
+                            <div class="box"><p>event.code</p> <div>${e.code}</div></div>`
 })
